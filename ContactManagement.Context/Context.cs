89186b0c64ec9e66b891047498e40515ee58f3e2
Felipe Pretti <felipepretti@gmail.com>
2023-05-21 22:35:35 -0300

    Reload test project

--- ContactManagement.Context/Context.cs
+++ ContactManagement.Context/Context.cs
@@ -7,10 +7,19 @@ namespace ContactManagement.Context
 {
     public class MariaDbContext : DbContext
     {
+        private IConfiguration @object;
+        private DbContextOptions<MariaDbContext> options;
+
         private IConfiguration Configuration { get; set; }
 
         public MariaDbContext()
-        { }      
+        { }
+
+        public MariaDbContext(IConfiguration configuration, DbContextOptions<MariaDbContext> options)
+            : base(options)
+        {
+            Configuration = configuration;
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -18,8 +27,16 @@ namespace ContactManagement.Context
             {
                 return;
             }
-            
-            optionsBuilder.UseMySQL("Server=localhost;Database=db;User Id=root;Password=root");
+
+            //For EF update-database command.
+            var connectionString = "Server=localhost;Database=db;User Id=root;Password=root";
+
+            if (Configuration is not null)
+            {
+                connectionString = Configuration.GetConnectionString("MariaDb");
+            }
+
+            optionsBuilder.UseMySQL();
         }
 
         public DbSet<Contact> Contact { get; set; }
